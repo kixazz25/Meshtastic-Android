d5c1e14ec6fb3bfe9af0e078e0ccbc901f148afe
kixazz25 <kixazz25@gmail.com>
2026-03-08 11:42:00 -0400

    A1.2: add TILE_SOURCES map and ACTIVE_TILE_SOURCE to ConvoyConfig

--- app/src/main/java/com/geeksville/mesh/convoy/ConvoyConfig.kt
+++ app/src/main/java/com/geeksville/mesh/convoy/ConvoyConfig.kt
@@ -2,6 +2,12 @@ package com.geeksville.mesh.convoy
 
 object ConvoyConfig {
     const val MAP_DEFAULT_ZOOM = 18.0
+    var TILE_SOURCES = mutableMapOf(
+        "SAT" to "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+        "TOPO" to "https://tile.opentopomap.org/{z}/{x}/{y}.png",
+        "RD" to "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+    )
+    var ACTIVE_TILE_SOURCE = "SAT" 
     const val MAP_GROUP_ZOOM_PADDING = 1.4f
     const val MAP_CART_ZOOM = 18.0
     const val MAP_MIN_ZOOM = 16.0
